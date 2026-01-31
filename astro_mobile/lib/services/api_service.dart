b3ddea534c4b2017e6633514e937ce97a8bffe1a
Argun Varan <argunvaran@gmail.com>
2026-01-31 11:38:20 +0300

    Fix syntax error: Remove duplicate rootUrl definition in ApiService

--- astro_mobile/lib/services/api_service.dart
+++ astro_mobile/lib/services/api_service.dart
@@ -266,13 +266,7 @@ class ApiService {
     return null;
   }
 
-  String get rootUrl {
-     if (kIsWeb) return "http://127.0.0.1:8000";
-     try {
-       if (Platform.isAndroid) return "http://10.0.2.2:8000";
-     } catch (_) {}
-     return "http://127.0.0.1:8000";
-  }
+
 
   // --- INTERACTIVE / SOCIAL API ---
 
